2e22d67464fa0e5f1dee4f83fbe81f53cb98da15
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-01-10 23:21:18 +0300

    initial commit

--- src/main/java/com/mehmetsukrukavak/HelloServlet.java
+++ src/main/java/com/mehmetsukrukavak/HelloServlet.java
@@ -1,4 +1,15 @@
 package com.mehmetsukrukavak;
 
-public class HelloServlet {
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        System.out.println("Hello Servlet");
+    }
 }
